bcc497cdc95becab21656c96c4414956dc41fe87
Kevin <33363013+fhdfa@users.noreply.github.com>
2017-12-12 23:08:16 -0800

    Added React-Bootstrap and some more minor setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 export default class App extends Component {
   render() {
     return (
       <div className="checkers-2000">
         <p>Checkers 2000</p>
+        <Button bsStyle="success">Test</Button>
       </div>
     );
   }
