0bcbe0c61684c44a2c506bde0fa922615db84713
fhdfa <33363013+fhdfa@users.noreply.github.com>
2018-03-06 17:03:01 -0800

    Calculations for possible mover highlight

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
-import { POSITIONS_PER_CHECKERBOARD, PIECES_PER_PLAYER } from './constants/checkerboard';
+import { POSITIONS_PER_ROW, POSITIONS_PER_CHECKERBOARD, PIECES_PER_PLAYER } from './constants/checkerboard';
 import { BLACK, RED } from './constants/colors';
-import { INITIAL_STATE, GAME_WON, GAME_ENDED } from './constants/gameStates';
+import { POSSIBLE_MOVER_HIGHLIGHT } from './constants/positionHighlights';
+import { INITIAL_STATE, SELECT_PIECE, GAME_WON, GAME_ENDED } from './constants/gameStates';
 import { GAME_CONTROL_ACTUATED } from './actions';
 
 const defaultPosition = {
@@ -16,6 +17,66 @@ const defaultState = {
   redPieces: 0,
   positions: new Array(POSITIONS_PER_CHECKERBOARD).fill(defaultPosition)
 };
+
+export default function reducer(state = defaultState, action) {
+  switch(action.type) {
+    case GAME_CONTROL_ACTUATED:
+      if(state.gameState === INITIAL_STATE)
+        return startGame(state);
+      else if(state.gameState === GAME_WON || state.gameState === GAME_ENDED)
+        return playAgain(state);
+      else
+        return endGame(state);
+    default:
+      return state;
+  }
+}
+
+function containsBlackPiece(position) {
+  return position.piece === BLACK;
+}
+
+function isEmptyPosition(position) {
+  return position !== null && position.piece === null;
+}
+
+function addPossibleMoverHighlight(thisPosition, forwardLeftPosition, forwardRightPosition) {
+  if(containsBlackPiece(thisPosition) && (isEmptyPosition(forwardLeftPosition) || isEmptyPosition(forwardRightPosition)))
+    return Object.assign({}, thisPosition, { highlight: POSSIBLE_MOVER_HIGHLIGHT });
+  else
+    return thisPosition;
+}
+
+function rowStartsRed(index) {
+  return index % (POSITIONS_PER_ROW * 2) < POSITIONS_PER_ROW;
+}
+
+function getForwardLeftPosition(index) {
+  if(rowStartsRed(index))
+    return index + POSITIONS_PER_ROW + 1;
+  else
+    return index + POSITIONS_PER_ROW;
+}
+
+function getForwardRightPosition(index) {
+  if(rowStartsRed(index))
+    return index + POSITIONS_PER_ROW;
+  else
+    return index + POSITIONS_PER_ROW - 1;
+}
+
+function someFunction(thisPosition, index, positions) {
+  const leftEdgePositions = [ 4, 12, 20, 28 ];
+  const rightEdgePositions = [ 3, 11, 19, 27 ];
+  const forwardLeftPosition = (rightEdgePositions.includes(index) ? null : positions[getForwardLeftPosition(index)]);
+  const forwardRightPosition = (leftEdgePositions.includes(index) ? null : positions[getForwardRightPosition(index)]);
+
+  return addPossibleMoverHighlight(thisPosition, forwardLeftPosition, forwardRightPosition);
+}
+
+function addAllPossibleMoverHighlights(positions) {
+  return positions.map(someFunction);
+}
 
 function positionsWithBlackPieces(positions) {
   return (
@@ -36,13 +97,19 @@ function positionsWithRedPieces(positions) {
 }
 
 function startGame(state) {
-  const nextPositions = [
+  const nextPositions = addAllPossibleMoverHighlights([
     ...positionsWithBlackPieces(state.positions),
     ...emptyPositions(state.positions),
     ...positionsWithRedPieces(state.positions)
-  ];
+  ]);
 
-  return Object.assign({}, state, { positions: nextPositions });
+  return Object.assign({}, state, {
+    gameState: SELECT_PIECE,
+    currentColor: BLACK,
+    blackPieces: PIECES_PER_PLAYER,
+    redPieces: PIECES_PER_PLAYER,
+    positions: nextPositions
+  });
 }
 
 function playAgain(state) {
@@ -51,18 +118,4 @@ function playAgain(state) {
 
 function endGame(state) {
   return state;
-}
-
-export default function reducer(state = defaultState, action) {
-  switch(action.type) {
-    case GAME_CONTROL_ACTUATED:
-      if(state.gameState === INITIAL_STATE)
-        return startGame(state);
-      else if(state.gameState === GAME_WON || state.gameState === GAME_ENDED)
-        return playAgain(state);
-      else
-        return endGame(state);
-    default:
-      return state;
-  }
 }
